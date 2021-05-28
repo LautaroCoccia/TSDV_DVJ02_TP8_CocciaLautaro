2a2e1945108cf820ad1ece904cbd66e0bb579c67
LautaroCoccia <lautaroauditore@gmail.com>
2021-05-27 22:42:51 -0300

    Add bricks generator

--- Assets/Scripts/LevelInitializer.cs
+++ Assets/Scripts/LevelInitializer.cs
@@ -4,10 +4,26 @@ using UnityEngine;
 
 public class LevelInitializer : MonoBehaviour
 {
+    [SerializeField] int height;
+    [SerializeField] int width;
+    [SerializeField] GameObject bricks;
     // Start is called before the first frame update
     void Start()
     {
-        
+        float spaceY = 0.25f;
+        float spaceX = 0f;
+        for(int i = 0; i<height; i++)
+        {
+
+            for (int j = 0; j < width; j++)
+            {
+               GameObject obj = Instantiate(bricks);
+               obj.transform.position = new Vector3((j * bricks.transform.localScale.x) + bricks.transform.localScale.x +1 + spaceX, 18 - i- spaceY, 0);
+                spaceX += 0.25f;
+            }
+            spaceY += 0.25f;
+            spaceX =0;
+        }
     }
 
     // Update is called once per frame
