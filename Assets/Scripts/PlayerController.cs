86ea384a7903b35a714b1fb49f2fe53341588c50
LautaroCoccia <lautaroauditore@gmail.com>
2021-05-29 14:50:40 -0300

    Add player collision with walls

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float movementSpeed = 2;
-
+    
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        transform.Translate(new Vector3(((Input.GetAxis("Horizontal") * movementSpeed) * Time.deltaTime), 0, 0));
+        float direction = Input.GetAxis("Horizontal");
+        if(CollisionChecker(direction))
+            transform.Translate(new Vector3(((direction * movementSpeed) * Time.deltaTime), 0, 0)) ; 
+    }
+
+    bool CollisionChecker(float direction)
+    {
+        RaycastHit hit;
+        return !(Physics.Raycast(transform.position, new Vector3(direction, 0, 0), transform.localScale.x/2));
+        
     }
 }
