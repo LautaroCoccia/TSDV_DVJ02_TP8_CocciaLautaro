d155bb1e1e796081d2411e481ee1520d8ce00809
LautaroCoccia <lautaroauditore@gmail.com>
2021-05-29 14:25:22 -0300

    Add player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed = 2;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(((Input.GetAxis("Horizontal") * movementSpeed) * Time.deltaTime), 0, 0));
+    }
+}
