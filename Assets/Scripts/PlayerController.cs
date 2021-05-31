3097422bde2a46661563b1f7eb43674d7e0d53f5
LautaroCoccia <lautaroauditore@gmail.com>
2021-05-30 21:56:38 -0300

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,18 +5,6 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float movementSpeed = 2;
-    [SerializeField] private GameObject ball;
-    [SerializeField] private int life = 3;
-    // Update is called once per frame
-    private void Update()
-    {
-        if(ball.transform.position.y<=0)
-        {
-            life--;
-            if (life <= 0)
-                Debug.Log("SE ACABO");
-        }
-    }
     void FixedUpdate()
     {
         float direction = Input.GetAxis("Horizontal");
